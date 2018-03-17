9c4204b6b8633f7c2ae5eeb3663c0cd12757d863
Jacobo Arcila Duque <jacoboarcila@hotmail.com>
2018-03-17 17:12:59 -0500

    Pinball 1.0

---
+++ JacoboArcila-Pinball/Assets/Scripts/Camera.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera : MonoBehaviour {
+
+	public GameObject ball;
+	private Vector3 offset;
+
+
+	// Use this for initialization
+	void Start () {
+		offset = transform.position - ball.transform.position;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		transform.position = ball.transform.position + offset;
+
+	}
+}
